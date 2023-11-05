55a1a026ee04dd3f02c79efd0750679e380fb427
Florian Goussin <floriangoussin@Florians-MacBook-Air.local>
2023-11-04 22:19:26 -0600

    work on book review

--- functions.php
+++ functions.php
@@ -44,20 +44,19 @@ add_action('wp_ajax_infinite_scroll', 'infinite_scroll_handler'); // logged in
 add_action('wp_ajax_nopriv_infinite_scroll', 'infinite_scroll_handler'); // logged out
 
 function load_more_js() {
-  if( is_singular( 'book' ) )
-    return;
+	if( is_singular( 'book' ) )
+		return;
 
-  $query = array( 
-  	'post_type' => 'book',
-    'posts_per_page' => 6,
-    'max_num_pages' => 3,
-    'order' => 'ASC'
-  );
-echo array_keys($wp_query);
-  $args = array(
-    'url'   => admin_url( 'admin-ajax.php' ),
-    'query' => $query,
-  );
+	$query = array( 
+		'post_type' => 'book',
+		'posts_per_page' => 6,
+		'max_num_pages' => 3,
+		'order' => 'ASC'
+	);
+	$args = array(
+	    'url' => admin_url( 'admin-ajax.php' ),
+	    'query' => $query,
+	);
 
   wp_enqueue_script( 'load-more', get_stylesheet_directory_uri() . '/js/load-more.js', array( 'jquery' ), '1.0', true );
   wp_localize_script( 'load-more', 'infinite_scroll_settings', $args );
@@ -66,5 +65,63 @@ echo array_keys($wp_query);
 add_action( 'wp_enqueue_scripts', 'load_more_js' );
 
 
+// Shortcode to allow the user to show a book review in a book post :)
+function book_review_shortcode($atts) {
+	$atts = shortcode_atts( [
+		'title' => '', // Book review title
+	], $atts );
+
+	if (empty($atts['title'])) {
+		return 'No title found in book_review.';
+	}
+
+	$bookId = get_the_ID(); 
+	$query = array( 
+		'post_type' => 'book-review',
+		'posts_per_page' => 1,
+		'max_num_pages' => 1,
+		'order' => 'ASC',
+		'meta_query' => array(
+			array(
+			    'key' => 'book_review_title',
+			    'value' => $atts['title'],
+			),
+			array(
+			    'key' => 'book_review_book',
+			    'value' => $bookId,
+			),
+	    )
+	);
+	$the_query = new WP_Query( $query );
+	while ( $the_query->have_posts() ) : $the_query->the_post();
+
+	$bookReviewTitle = get_field('book_review_title');
+	$bookReviewContent = get_field('book_review_content');
+
+    $output .= 
+    	'<div class="book-review">'                             
+    		.'<div class="book-review_content">' 
+    			. '<blockquote>' . $bookReviewContent . '</blockquote> - <cite>' . $bookReviewTitle . '</cite>'
+    		. '</div>'
+    	.'</div>';
+
+    endwhile;
+    wp_reset_postdata();
+
+	return $output;
+}
+add_shortcode( 'book_review', 'book_review_shortcode' );
+
+
+function book_review_publish_handler( $post_id ) {
+	if ( get_post_type($post_id) != 'book-review' ) {
+		return;
+	}
+	$post = get_post($post_id);
+	$post_content = trim(strip_tags(apply_filters('the_content', $post->post_content)));
+	update_post_meta($post_id, 'book_review_title', $post->post_title);
+	update_post_meta($post_id, 'book_review_content', $post_content);
+}
+add_action( 'save_post', 'book_review_publish_handler' );
 
 
