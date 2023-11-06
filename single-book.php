d56545085bc7a76098294aa207e9569e6f99ca2d
Florian Goussin <floriangoussin@Florians-MacBook-Air.local>
2023-11-05 20:55:45 -0700

    Fix date and link to publisher

--- single-book.php
+++ single-book.php
@@ -17,14 +17,33 @@
 			    	// Use the traditional Wordpress way to display the post details and additionally get some specific fields
 			    	if (have_posts()) : while (have_posts()) : the_post(); 
 			    	$bookTitle = get_field('book_title');
-			    	$bookPublisher = get_field('book_publisher');
+					$bookPublishDateFormat = get_field('book_publish_date_format');
+
+					// strtotime to convert string to timestamp and then wp_date to format date from timestamp
+					$bookPublishDate = wp_date($bookPublishDateFormat, strtotime(get_field('book_publish_date')));
 			    ?>
 			    	<div class="single-book">
 			    		<div class="book-header">
 							<?php if (!empty($bookTitle)): ?>
 								<h1 class="post-title"><?php echo $bookTitle; ?></h1>
 							<?php endif; ?>
-							<div class="book-publisher"><?php echo $bookPublisher; ?></div>
+							<?php
+							$bookPublisher = get_field('book_publisher');
+							if( $bookPublisher ):
+								$link_url = $bookPublisher['url'];
+							    $link_title = $bookPublisher['title'];
+							    $link_target = $bookPublisher['target'] ? $bookPublisher['target'] : '_self';
+							?>
+								<a 
+									class="publisher-link" 
+									href="<?php echo esc_url( $link_url ); ?>" 
+									target="<?php echo esc_attr( $link_target ); ?>"
+								>
+									<?php echo esc_html( $link_title ); ?>
+								</a>
+							<?php endif; ?>
+							 - 
+							<span class="book-publishing-date"><?php echo $bookPublishDate; ?></span>
 						</div>
 
 						<?php the_content();?>
